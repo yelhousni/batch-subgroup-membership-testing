1a91c2f859279200ba558e8290bbf97fc1835d14
Youssef El Housni <youssef.housni21@gmail.com>
2025-06-12 14:52:51 -0400

    fix amd asm

--- bls12377-strong/internal/fptower/e2_bls377.go
+++ bls12377-strong/internal/fptower/e2_bls377.go
@@ -88,3 +88,24 @@ func (z *E2) MulBybTwistCurveCoeff(x *E2) *E2 {
 
 	return z
 }
+
+// MulByNonResidue multiplies a E2 by (1,1)
+func (z *E2) MulByNonResidue(x *E2) *E2 {
+	var a fp.Element
+	a.Sub(&x.A0, &x.A1)
+	z.A1.Add(&x.A0, &x.A1)
+	z.A0.Set(&a)
+	return z
+}
+
+// Mul sets z to the E2-product of x,y, returns z
+func (z *E2) Mul(x, y *E2) *E2 {
+	mulGenericE2(z, x, y)
+	return z
+}
+
+// Square sets z to the E2-product of x,x returns z
+func (z *E2) Square(x *E2) *E2 {
+	squareGenericE2(z, x)
+	return z
+}
